5552da38cb0b2e2aa17774615643d7c142e8529b
truongkhongsai <anhp31746@gmail.com>
2023-07-14 23:54:49 +0700

    fist commit

---
+++ Dog.java
@@ -0,0 +1,10 @@
+class Dog extends Animal implements Run {
+    public void eat() {
+        System.out.println("ăn 5 bát ");
+    }
+
+    public void runer() {
+        System.out.println("chạy nhảy  ");
+    }
+
+}
---
+++ Run.java
@@ -0,0 +1,3 @@
+interface Run {
+    public void runer();
+}
